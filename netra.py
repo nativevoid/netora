04e5e4d383fb216915174e180c03d4b236986bc6
nativevoid <baileyn953@gmail.com>
2025-04-03 12:47:46 +0100

    Initial commit

--- netra.py
+++ netra.py
@@ -3,7 +3,7 @@ import requests
 import sys
 import time
 
-from colorama import Style, Fore
+from colorama import Style, Fore, init
 
 from src.modules.core.getinfo import get_ip_info
 from src.modules.utils.initialize import initialize
@@ -33,182 +33,211 @@ def main():
         )
         parser.add_argument(
             "ip",
-            metavar="IP_ADDRESS",
+            metavar="IP_ADDRESSES",
+            nargs="+",
             action="store",
-            help="IP address for location and network info lookup.",
+            help="One or more IP addresses for looking up location and network information.",
         )
         parser.add_argument(
-            "-o",
-            "--output", 
-            type=str, 
-            help="Output file to save results.",
+            "--folderoutput",
+            "-fo",
+            dest="folderoutput",
+            help="Multiple IP addresses can be used, and the results will be saved in this folder."
+        )    
+        parser.add_argument(
+            "--output",
+            "-o", 
+            dest="output", 
+            help="Only one IP address can be used, and the result will be saved to this file.",
+        )
+        parser.add_argument(
+            "--no-color",
+            action="store_true",
+            dest="no_color",
+            default=False,
+            help="Don't color terminal output",
         )
 
         args = parser.parse_args()
-
-        target_ip = args.ip  
         export_file = args.output
 
-        time.sleep(2)
-        if not validate_ip(target_ip):
+        if args.no_color:
+            init(strip=True, convert=False)
+        else:
+            init(autoreset=True)
+
+        if args.output is not None and len(args.ip) != 1:
             print(
                 Fore.WHITE + Style.BRIGHT + "\n["
                 + Fore.RED + "!" + Fore.WHITE + "]"
                 + Fore.RED + " Error:"
-                + Style.RESET_ALL + " Invalid IP address" 
+                + Fore.WHITE + " You can only use --output with a single username" 
+                + Style.RESET_ALL
             )
             sys.exit(1)
 
-        print(
-            Fore.GREEN + Style.BRIGHT + "["
-            + Fore.YELLOW + "*" 
-            + Fore.GREEN + "]"
-            + " Retrieving" + Fore.WHITE 
-            + " IP" + Fore.GREEN + " information"
-            + Style.RESET_ALL
-        )
+        for target_ip in args.ip:
+            time.sleep(2)
+            if not validate_ip(target_ip):
+                print(
+                    Fore.WHITE + Style.BRIGHT + "\n["
+                    + Fore.RED + "!" + Fore.WHITE + "]"
+                    + Fore.RED + " Error:"
+                    + Fore.WHITE  + " Invalid IP address" 
+                    + Style.RESET_ALL
+                )
+                continue
 
-        response = requests.get(f"https://ipapi.co/{target_ip}/json")
-        response2 = requests.get(f"https://api.db-ip.com/v2/free/{target_ip}")
-
-        if response.status_code == 200 and response2.status_code == 200:
-            ip_info = response.json()
-            ip_info2 = response2.json()
-
-            ip_data = get_ip_info(ip_info, ip_info2)
-            ip_location = f"https://www.openstreetmap.org/?mlat={ip_data['latitude']}&mlon={ip_data['longitude']}"
-            ip_location_response = requests.get(ip_location, headers=headers)
-
-            if ip_location_response.status_code == 200:          
-                ip_data = get_ip_info(ip_info, ip_info2, ip_location)               
-            else:
-                ip_location = "Not Found"
-                ip_data = get_ip_info(ip_info, ip_info, ip_location)
-
-            results_count = len(ip_data)
-
-            for ip, info in ip_data.items():
-                if "Not Found" in str(info):
-                    results_count -= 1
-
-            time.sleep(2.5)
             print(
-                Fore.WHITE + Style.BRIGHT + "\n[" 
-                + Fore.GREEN + "+" 
-                + Fore.WHITE + "]"
-                + Fore.GREEN + " Data"
-                + Fore.WHITE + " retrieved successfully"
+                Fore.GREEN + Style.BRIGHT + "["
+                + Fore.YELLOW + "*" 
+                + Fore.GREEN + "]"
+                + " Retrieving" + Fore.WHITE 
+                + " IP" + Fore.GREEN + " information"
                 + Style.RESET_ALL
             )
-            time.sleep(1.5)
 
-            print(
-                Fore.WHITE + Style.BRIGHT + "\n\n" + "[" 
-                + Fore.GREEN + "+" 
-                + Fore.WHITE + "]"
-                + Fore.GREEN + " Target:" 
-                + Style.RESET_ALL +  f" {ip_data['ip']}" + "\n" + 
+            response = requests.get(f"https://ipapi.co/{target_ip}/json")
+            response2 = requests.get(f"https://api.db-ip.com/v2/free/{target_ip}")
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]"
-                + Fore.GREEN + " IP version:" 
-                + Style.RESET_ALL + f" {ip_data['ipv']}" + "\n" +
+            if response.status_code == 200 and response2.status_code == 200:
+                ip_info = response.json()
+                ip_info2 = response2.json()
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]"
-                + Fore.GREEN + " ISP:" 
-                + Style.RESET_ALL + f" {ip_data['isp']}" + "\n" +
+                ip_data = get_ip_info(ip_info, ip_info2)
+                ip_location = f"https://www.openstreetmap.org/?mlat={ip_data['latitude']}&mlon={ip_data['longitude']}"
+                ip_location_response = requests.get(ip_location, headers=headers)
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]"
-                + Fore.GREEN + " Continent:" 
-                + Style.RESET_ALL + f" {ip_data['continent_name']}" + "\n" +
+                if ip_location_response.status_code == 200:          
+                    ip_data = get_ip_info(ip_info, ip_info2, ip_location)               
+                else:
+                    ip_location = "Not Found"
+                    ip_data = get_ip_info(ip_info, ip_info, ip_location)
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]"
-                + Fore.GREEN + " Continent code:" 
-                + Style.RESET_ALL + f" {ip_data['continent_code']}" + "\n" +
+                results_count = len(ip_data)
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]"
-                + Fore.GREEN + " Country:" 
-                + Style.RESET_ALL + f" {ip_data['country']}" + "\n" +
+                for ip, info in ip_data.items():
+                    if "Not Found" in str(info):
+                        results_count -= 1
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]"
-                + Fore.GREEN + " Country code:" 
-                + Style.RESET_ALL + f" {ip_data['country_code']}" + "\n" +
+                time.sleep(2.5)
+                print(
+                    Fore.WHITE + Style.BRIGHT + "\n[" 
+                    + Fore.GREEN + "+" 
+                    + Fore.WHITE + "]"
+                    + Fore.GREEN + " Data"
+                    + Fore.WHITE + " retrieved successfully"
+                    + Style.RESET_ALL
+                )
+                time.sleep(1.5)
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]" 
-                + Fore.GREEN + " Country capital:" 
-                + Style.RESET_ALL + f" {ip_data['country_capital']}" + "\n" +
+                print(
+                    Fore.WHITE + Style.BRIGHT + "\n\n" + "[" 
+                    + Fore.GREEN + "+" 
+                    + Fore.WHITE + "]"
+                    + Fore.GREEN + " Target:" 
+                    + Style.RESET_ALL +  f" {ip_data['ip']}" + "\n" + 
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]" 
-                + Fore.GREEN + " Phone code:" 
-                + Style.RESET_ALL + f" {ip_data['phone_code']}" + "\n" +
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]"
+                    + Fore.GREEN + " IP version:" 
+                    + Style.RESET_ALL + f" {ip_data['ipv']}" + "\n" +
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]" 
-                + Fore.GREEN + " City:" 
-                + Style.RESET_ALL + f" {ip_data['city']}" + "\n" +
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]"
+                    + Fore.GREEN + " ISP:" 
+                    + Style.RESET_ALL + f" {ip_data['isp']}" + "\n" +
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]" 
-                + Fore.GREEN + " Latitude:" 
-                + Style.RESET_ALL + f" {ip_data['latitude']}" + "\n" +
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]"
+                    + Fore.GREEN + " Continent:" 
+                    + Style.RESET_ALL + f" {ip_data['continent_name']}" + "\n" +
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]" 
-                + Fore.GREEN + " Longitude:" 
-                + Style.RESET_ALL + f" {ip_data['longitude']}" + "\n" +
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]"
+                    + Fore.GREEN + " Continent code:" 
+                    + Style.RESET_ALL + f" {ip_data['continent_code']}" + "\n" +
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]" 
-                + Fore.GREEN + " Location link:" 
-                + Style.RESET_ALL + f" {ip_data['location_link']}" + "\n" +
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]"
+                    + Fore.GREEN + " Country:" 
+                    + Style.RESET_ALL + f" {ip_data['country']}" + "\n" +
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]" 
-                + Fore.GREEN + " Currency name:" 
-                + Style.RESET_ALL + f" {ip_data['currency_name']}" + "\n" +
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]"
+                    + Fore.GREEN + " Country code:" 
+                    + Style.RESET_ALL + f" {ip_data['country_code']}" + "\n" +
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]" 
-                + Fore.GREEN + " Currency symbol:" 
-                + Style.RESET_ALL + f" {ip_data['currency_symbol']}" + "\n" +
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]" 
+                    + Fore.GREEN + " Country capital:" 
+                    + Style.RESET_ALL + f" {ip_data['country_capital']}" + "\n" +
 
-                Fore.WHITE + Style.BRIGHT + "["
-                + Fore.GREEN + "+"
-                + Fore.WHITE + "]" 
-                + Fore.GREEN + " Local time:" 
-                + Style.RESET_ALL + f" {ip_data['local_time']}" + "\n\n" +
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]" 
+                    + Fore.GREEN + " Phone code:" 
+                    + Style.RESET_ALL + f" {ip_data['phone_code']}" + "\n" +
+
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]" 
+                    + Fore.GREEN + " City:" 
+                    + Style.RESET_ALL + f" {ip_data['city']}" + "\n" +
+
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]" 
+                    + Fore.GREEN + " Latitude:" 
+                    + Style.RESET_ALL + f" {ip_data['latitude']}" + "\n" +
+
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]" 
+                    + Fore.GREEN + " Longitude:" 
+                    + Style.RESET_ALL + f" {ip_data['longitude']}" + "\n" +
+
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]" 
+                    + Fore.GREEN + " Location link:" 
+                    + Style.RESET_ALL + f" {ip_data['location_link']}" + "\n" +
+
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]" 
+                    + Fore.GREEN + " Currency name:" 
+                    + Style.RESET_ALL + f" {ip_data['currency_name']}" + "\n" +
+
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]" 
+                    + Fore.GREEN + " Currency symbol:" 
+                    + Style.RESET_ALL + f" {ip_data['currency_symbol']}" + "\n" +
+
+                    Fore.WHITE + Style.BRIGHT + "["
+                    + Fore.GREEN + "+"
+                    + Fore.WHITE + "]" 
+                    + Fore.GREEN + " Local time:" 
+                    + Style.RESET_ALL + f" {ip_data['local_time']}" + "\n\n" +
                 
-                Fore.GREEN + Style.BRIGHT + "["
-                + Fore.YELLOW + "*"
-                + Fore.GREEN + "]"
-                + f" IP lookup completed with {Fore.WHITE}{results_count}{Fore.GREEN} results" 
-                + Style.RESET_ALL,
-                flush=True
-            )
+                    Fore.GREEN + Style.BRIGHT + "["
+                    + Fore.YELLOW + "*"
+                    + Fore.GREEN + "]"
+                    + f" IP lookup completed with {Fore.WHITE}{results_count}{Fore.GREEN} results" 
+                    + Style.RESET_ALL,
+                    flush=True
+                )
 
-            if export_file is not None:
-                export_info(export_file, ip_data)
+                if export_file is not None:
+                    export_info(export_file, ip_data, results_count)
 
     except KeyboardInterrupt:
         print(
--- src/modules/utils/export.py
+++ src/modules/utils/export.py
@@ -1,4 +1,4 @@
-def export_info(export_file, info):
+def export_info(export_file, info, results_count):
     with open(export_file, "w") as f:
 
         for key, value in info.items():
@@ -6,3 +6,4 @@ def export_info(export_file, info):
                 value = f"{value:.4f}"
                                
             f.write(f"{key}: {value}\n")
+        f.write(f"\n[*] IP lookup completed with {results_count} results")
